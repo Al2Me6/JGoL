5009f17637c8d472392567c14aa8e795649dca1d
Alvin Meng <alvindotd@gmail.com>
2019-05-08 11:54:17 -0400

    added button size slider, broken.

--- src/UI.java
+++ src/UI.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 public class UI extends JFrame {
     private Board board;
     private ButtonGrid buttonGrid;
+    private final int INITIAL_BUTTON_SIZE = 15;
 
     public UI() {
         board = new Board(userInput("Board width:"), userInput("Board height"));
@@ -31,7 +32,7 @@ public class UI extends JFrame {
             buttons = new CellButton[board.getHeight()][board.getWidth()];
             for (int i = 0; i < board.getHeight(); i++) {
                 for (int j = 0; j < board.getWidth(); j++) {
-                    buttons[i][j] = new CellButton(new Coordinate(i, j), 15);
+                    buttons[i][j] = new CellButton(new Coordinate(i, j), INITIAL_BUTTON_SIZE);
                     gridPanel.add(buttons[i][j]);
                 }
             }
@@ -44,7 +45,7 @@ public class UI extends JFrame {
 
             public CellButton(Coordinate c, int size) {
                 coordinate = c;
-                setButtonSize(size);
+                setSize(size);
                 setOpaque(true);
                 colorize();
                 addActionListener(e -> {
@@ -57,35 +58,50 @@ public class UI extends JFrame {
                 setBackground(board.getCellState(coordinate) ? Color.BLACK : Color.WHITE);
             }
 
-            public void setButtonSize(int px) {
+            public void setSize(int px) {
                 setPreferredSize(new Dimension(px, px));
             }
         }
 
-        public void colorizeButton(Coordinate c) {
+        public void colorizeButtons(Coordinate c) {
             buttons[c.x()][c.y()].colorize();
+        }
+
+        public void setButtonSize(int size) {
+            for (CellButton[] row : buttons) {
+                for (CellButton b : row)
+                    b.setSize(size);
+            }
         }
     }
 
     /**
-     * UI Components with settings control Includes buttons for clearing, evolving, and autoevolving
+     * UI Components with settings control Includes buttons for clearing, evolving,
+     * and autoevolving
      */
     private class Controls extends JPanel {
         private JButton autoevolve;
         private JLabel generationCounter;
+        private JSlider sizeSlider;
 
         /**
-         * The Constructor for controls
-         * Adds all the buttons
+         * The Constructor for controls Adds all the buttons
          */
         public Controls() {
+            setLayout(new GridLayout(3, 1));
+            setSize(200, 100);
+            // setPreferredSize(new Dimension(200, 100));
+
+            Panel controlButtonsPanel = new Panel(new FlowLayout());
+            add(controlButtonsPanel);
+
             JButton nextGen = new JButton("Evolve state");
             // On button click, evolve the board once
             nextGen.addActionListener(e -> {
                 updateBoard(board.evolve());
                 updateGenCounter();
             });
-            add(nextGen);
+            controlButtonsPanel.add(nextGen);
 
             JButton clear = new JButton("Clear board");
             // On button click, clear the board
@@ -93,17 +109,28 @@ public class UI extends JFrame {
                 updateBoard(board.clear());
                 updateGenCounter();
             });
-            add(clear);
+            controlButtonsPanel.add(clear);
 
             autoevolve = new JButton("Autoevolve");
             // On button click, start auto evolve
             autoevolve.addActionListener(e -> {
             });
-            add(autoevolve);
+            controlButtonsPanel.add(autoevolve);
 
             generationCounter = new JLabel();
             updateGenCounter();
             add(generationCounter);
+
+            sizeSlider = new JSlider(JSlider.HORIZONTAL, 5, 30, INITIAL_BUTTON_SIZE);
+            sizeSlider.setMinorTickSpacing(1);
+            sizeSlider.setMajorTickSpacing(5);
+            sizeSlider.setPaintTicks(true);
+            sizeSlider.setPaintLabels(true);
+            sizeSlider.addChangeListener(e -> {
+                buttonGrid.setButtonSize(((JSlider) e.getSource()).getValue());
+                refresh();
+            });
+            add(sizeSlider);
         }
 
         /**
@@ -115,7 +142,7 @@ public class UI extends JFrame {
 
         private void updateBoard(HashSet<Coordinate> delta) {
             for (Coordinate c : delta)
-                buttonGrid.colorizeButton(c);
+                buttonGrid.colorizeButtons(c);
         }
     }
 
@@ -124,8 +151,14 @@ public class UI extends JFrame {
             try {
                 return Integer.parseInt(JOptionPane.showInputDialog(null, question));
             } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Please input a valid integer!", "Input Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Please input a valid integer!", "Input Error",
+                        JOptionPane.ERROR_MESSAGE);
             }
         }
+    }
+
+    private void refresh() {
+        revalidate();
+        repaint();
     }
 }
